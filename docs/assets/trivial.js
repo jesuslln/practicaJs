c8250a9653be0c181888b7b06e06fc0987deb8be
Jesuslln <201910180@alu.comillas.edu>
2022-02-28 00:15:04 +0000

    practicaJs

--- docs/assets/trivial.js
+++ docs/assets/trivial.js
@@ -5,23 +5,30 @@ let first_opt = document.getElementById("lbl1");
 let second_opt = document.getElementById("lbl2");
 let third_opt = document.getElementById("lbl3");
 let fourth_opt = document.getElementById("lbl4");
+let topic = document.getElementById("topic");
+console.log(topic);
+let api_return;
+let question;
 
 
 async function getQuestion(){
     //api request GET
-    const response = await fetch(url);
+    const response = await fetch(getApiUrl());
     const data = await response.json();
-    console.log(data.results);
 
 
     // Displaying question
-    let question = document.createTextNode(data.results[0].question);
+    question = document.createTextNode(data.results[0].question);
+    question = cleanQuestion(question);
     element.innerHTML= "";
-    element.appendChild(question);
+    element.innerHTML += question.nodeValue;    //nodeValue is required to pass a string to the function
+
 
     //Displaying Anwers - Answers positions should be randomized
     let incorrect_answers = data.results[0].incorrect_answers;
     let correct_answer = data.results[0].correct_answer;
+    console.log(correct_answer);
+    console.log(typeof correct_answer);
 
     first_opt.innerHTML = "";
     first_opt.innerHTML += correct_answer;
@@ -36,8 +43,8 @@ async function getQuestion(){
 
 function checkAnswer(){
     console.log(document.getElementsByName("option"));
-    console.log(document.getElementsById("option1"));
-    if(document.getElementsById("option").checked){
+    console.log(document.getElementById("option1"));
+    if(document.getElementById("option1").checked){
         alert("Eh correcto!");
     } else{
         alert("No eh correcto");
@@ -45,3 +52,21 @@ function checkAnswer(){
 }
  
 
+function getApiUrl(){
+   if(topic.selectedIndex != 0){
+       api_return = url + "&category=" + (topic.selectedIndex+8);
+   } else{
+       api_return = url;
+   }
+   console.log(api_return + "-" + topic.selectedIndex);
+    return api_return;
+}
+
+function cleanQuestion(question){
+    if(question.toString().search("&quote;")==-1){
+        question.toString().replace("&quote;", "'");
+    }
+    return question;
+}
+
+function randomizeAnswers(){}
